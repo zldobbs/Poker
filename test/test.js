3e7a95e3d5ada2eaea498c18bf15afd7d0e75133
Zach <dobbszach@gmail.com>
2018-03-18 21:32:53 -0500

    implement testing foundation

---
+++ test/test.js
@@ -0,0 +1,12 @@
+// test cases
+var assert = require('assert');
+describe('Game Flow', function() {
+  // the player whose turn it is should always display controls
+  it('displays controls properly', function() {
+    // NOTE this probably isn't going to work without async...
+    // setup the basic foundation, can definitely find other unit cases
+
+    // will revisit soon
+    
+  });
+});
